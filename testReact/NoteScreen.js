e76f14548f7f0a91732942c250ff65d2c534a6cb
ariadiprana <ari.adiprana@gmail.com>
2016-11-03 10:18:32 +0700

    release

--- testReact/NoteScreen.js
+++ testReact/NoteScreen.js
@@ -22,6 +22,9 @@ class NoteScreen extends Component{
   render(){
     return(
       <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.mainSection}>
+          <Image>
+        </View>
       </ScrollView>
     )
   }
@@ -44,6 +47,14 @@ const styles = StyleSheet.create({
     flexDirection:'row'
   },
   detailsImage:{
-    width:134
+    width:134,
+    height:200,
+    backgroundColor:'#eaeaea',
+    marginRight: 10
+  },
+  separator: {
+    backgroundColor:'rgba(0,0,0,0.1)',
+    height: StyleSheet.hairlineWidth,
+    marginVertical: 10
   }
 })
